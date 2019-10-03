b05d0462fcceaa0a1b9da47607926002845a6118
Sharik Gurung <SGurung@spartaglobal.com>
2019-10-03 12:37:02 +0100

    pseudo coded the pyfiles

---
+++ db_class.py
@@ -0,0 +1,12 @@
+# define a class to access our db
+
+    # have a characteristics to access the db
+
+    # methods
+
+    # all() # should be a class method
+        # gets all the instances from DB
+        # get each records
+        # create individual instances of recipe
+        # store them in a list
+        # return_list
---
+++ recipe_class.py
@@ -0,0 +1,22 @@
+# define a class recipe
+
+
+    # Give it the needed attributes
+
+
+    # start defining methods
+
+
+    # new ()
+        # creates a recipe object
+
+    # save()
+        # saves a recipe object to the DB (make persistent)
+
+    # read() have this one on the database class
+        # reads one object
+
+
+
+    # Destroy
+        # one object
